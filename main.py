1fb7c26af2bb8ee15a68532a1c22c1c274110a9b
Arthur <asad.artur@gmail.com>
2020-09-16 20:55:51 +0300

    add deco

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from . import static
+import static
+
+import telebot
 
 bot_api = static.bot_api
+
+@bot.message_handler(content_types=['text'])
+
+if message.text == "Привет":
+    bot.send_message(message.from_user.id, "Привет, для просмотра правил игры введи /help ")
+elif message.text == "/help":
+    bot.send_message(message.from_user.id, "Напиши привет")
+else:
+    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+#def game_info():
+    
+
+
+
+#def game(message):
